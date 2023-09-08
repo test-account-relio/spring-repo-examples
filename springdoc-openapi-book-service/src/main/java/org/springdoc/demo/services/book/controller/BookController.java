a69a944243925625403d2a3a809917915e166de3
devpaltestaccount <117631422+devpaltestaccount@users.noreply.github.com>
2023-09-08 10:59:20 +0300

    Breaking Changes 1

--- springdoc-openapi-book-service/src/main/java/org/springdoc/demo/services/book/controller/BookController.java
+++ springdoc-openapi-book-service/src/main/java/org/springdoc/demo/services/book/controller/BookController.java
@@ -61,7 +61,7 @@ public class BookController {
 
 	@PutMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public Book updateBook(@PathVariable("id") final String id, @RequestBody final Book book) {
+	public Book updateBook(@PathVariable("id") final String id /*, @RequestBody final Book book*/) {
 		return book;
 	}
 
